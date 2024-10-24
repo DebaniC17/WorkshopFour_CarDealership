5288c700f117b7b737da90d508bc05340a2c1c50
Debani <debani.cossio1217@gmail.com>
2024-10-24 17:37:26 -0500

    added UserInterface, Vehicle, Dealership classes and inventory.csv file

---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,17 @@
+package com.ps;
+
+public class UserInterface {
+
+    //private static Dealership dealership;
+
+    public static void init() {
+        System.out.println("Init method");
+        //loading the dealership and inventory from a file
+    }
+
+   public static void display() {
+       init();
+       System.out.println('load menu');
+       //load the menu
+   }
+}
